a5ab40f6a6d83d20e73dacc753e8b1d4e6e7e07b
Alex Kavourias <Kavourias.a@husky.neu.edu>
2015-08-30 13:31:07 -0400

    implemented insertObject

--- database/models/Match.java
+++ database/models/Match.java
@@ -1,14 +1,20 @@
 package database.models;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.*;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Alex on 8/28/2015.
  */
 public class Match extends AbstractModel {
     public static final String tableName = "Matches";
-    public final List<String> COLUMNS = Arrays.asList("matchId", "datePlayed", "winner", "lengthSeconds");
+    public final List<String> COLUMNS = Arrays.asList("id", "datePlayed", "region", "winningTeAM", "length");
     private int matchId;
     private int lengthSeconds;
     private Date datePlayed;
@@ -42,6 +48,20 @@ public class Match extends AbstractModel {
 
     @Override
     public boolean insertObject(AbstractModel toInsert) throws IllegalArgumentException {
-        return false;
+        if (!this.verifyMap(toInsert.toMap(), true)) {
+            return false;
+        }
+        try {
+            String query = String.format("INSERT INTO %s (id, datePlayed, winningTeam, length) VALUES(?, ?, ?, ?)", tableName);
+            PreparedStatement st = conn.prepareStatement(query);
+            st.setInt(1, (Integer) toInsert.getValue("id"));
+            st.setDate(2, (Date) toInsert.getValue("date"));
+            st.setString(3, (String) toInsert.getValue("winningTeam"));
+            st.setInt(4, (Integer) toInsert.getValue("length"));
+            return st.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
