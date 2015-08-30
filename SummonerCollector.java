842ecb6113e07ae1d7c945b29de941b0d4798d95
Alex Kavourias <Kavourias.a@husky.neu.edu>
2015-08-30 13:30:21 -0400

    Add method for collecting Summoner information from Challenger ladder

--- SummonerCollector.java
+++ SummonerCollector.java
@@ -1,3 +1,9 @@
+import constant.Region;
+import database.models.Summoner;
+import dto.League.League;
+import dto.League.LeagueEntry;
+import main.java.riotapi.RiotApiException;
+
 /**
  * Created by Alex on 8/28/2015.
  */
@@ -7,7 +13,24 @@ public class SummonerCollector extends AbstractCollector {
         super(apiKey);
     }
 
-    public void collectChallengerLadder() {
-        //TODO
+    public void collectChallengerLadder() throws RiotApiException {
+        League league = this.api.getChallengerLeagues();
+        for (Region r : Region.values())
+            for (LeagueEntry entry : league.getEntries()) {
+                    new Summoner().insertObject(new Summoner(
+                            Integer.parseInt(entry.getPlayerOrTeamId()),
+                            api.getRegion(),
+                            entry.getPlayerOrTeamName(),
+                            entry.getLeaguePoints(),
+                            entry.getDivision()));
+                }
+    }
+
+    /**
+     * Method to update info for summoners, filtering out ones that already exist
+     * TODO
+     */
+    public void updateChallengeLadderInfo() {
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 }
