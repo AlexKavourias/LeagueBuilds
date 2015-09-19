c0174ca4b5f77dac5cc91007ddbd67e997d6df4f
Alex Kavourias <Kavourias.a@husky.neu.edu>
2015-09-19 15:11:25 -0400

    Added method for collecting masteries and rune pages.  Still need to add method for parsing the json

--- MatchCollector.java
+++ MatchCollector.java
@@ -1,23 +1,41 @@
-
-
-import database.models.Match;
+import constant.Region;
+import database.models.Queryable;
 import database.models.Summoner;
 
-import java.util.AbstractCollection;
-import java.util.List;
+import main.java.riotapi.Request;
+import main.java.riotapi.RiotApiException;
 
-/**
- * Created by Alex on 8/30/2015.
- */
+import java.util.*;
+
 public class MatchCollector extends AbstractCollector {
-
     public MatchCollector(String apiKey) {
         super(apiKey);
+        this.apiKey = apiKey;
     }
 
-    public List<Match> getSummonerMatches(Summoner summoner) {
-
-        api.getMatchList((long) )
+    public void collectMasteries() throws RiotApiException {
+        for (Region region: Region.values()) {
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("Region", region.getName());
+            List<Integer> summonerIds = new ArrayList<>();
+            //Put summoner ids for all summoners in the region into a comma separated string for api call
+            List<Integer> ids = new ArrayList<>();
+            new Summoner().findObject(new Summoner(map)).forEach(s -> ids.add((Integer) s.getValue("id")));
+            while (!ids.isEmpty()) {
+                StringBuilder idString = new StringBuilder();
+                int count = 0;
+                while (count++ < maxIDs ) {
+                    if (ids.isEmpty())
+                        break;
+                    idString.append(ids.remove(0));
+                    idString.append(",");
+                }
+                String url = String.format(baseURL,
+                        region.getName(), "summoner", idString, "masteries", apiKey);
+                System.out.println(url);
+                System.out.println(Request.execute(String.format(baseURL,
+                        region.getName(), "summoner", idString, "masteries", apiKey)));
+            }
+        }
     }
-
 }
